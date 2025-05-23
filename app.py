f4cc5259fb59725ddc289fe8813404494cace827
moluck_ <woe877@haw-hamburg.de>
2025-05-23 14:42:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -79,13 +79,13 @@ if st.button("📄 PDF generieren") and survey_id:
 
                 # Antwortoptionen abfragen
                 answer_map = {}
-                awnsers_payload_pre = {
+                answers_payload_pre = {
                     "method": "list_answers",
                     "params": [session_key, survey_id],
                     "id": 3
                 }
                 answers_response_pre = requests.post(LS_URL, json=answers_payload_pre)
-                st.code(awnsers_payload_pre)
+                st.code(answers_response_pre)
                 for q in questions:
                     st.write(q["title"], q["qid"], q["question"])
                     answers_payload = {
